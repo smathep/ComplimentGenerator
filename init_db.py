399e34e69759815f4196572ff24bc64bc23ffad7
Patrick Smathers <patricksmathers@icloud.com>
2022-12-20 15:10:49 +0000

    Added more compliments. Added beatles reference

--- init_db.py
+++ init_db.py
@@ -15,32 +15,35 @@ def insert_author(lname, fname):
 def insert_compliment(lname, fname, comp):
     author = cur.execute('SELECT author_id FROM authors WHERE f_name = ? AND l_name = ?', (fname, lname)).fetchall()[0][0]
     # print(f'author_id: {author}')
-    cur.execute("INSERT INTO compliments (author_id, content) VALUES (?, ?)",
-            (author, comp))
+    cur.execute("INSERT INTO compliments (author_id, content) VALUES (?, ?)", (author, comp))
+
 # Add authors into DB
 insert_author('Smathers', 'Patrick')
 insert_author('Smith', 'Corinne')
 insert_author('Collins', 'Katelynne')
-# cur.execute("INSERT INTO authors (l_name, f_name) VALUES (?, ?)",
-#             ('Smathers', 'Patrick'))
-# cur.execute("INSERT INTO authors (l_name, f_name) VALUES (?, ?)",
-#             ('Smith', 'Corrine'))
+insert_author('Pagilagan', 'Karl')
+insert_author('Taylor', 'Caden')
+
 
 #Add compliments
 # insert_compliment('Smathers', 'Patrick', 'The message would go here. Can be something short or a few sentences.')
 
-insert_compliment('Smathers', 'Patrick', "Hi Alana :)\n\
-            I hope you know just how sweet and amazing you are. You help so many families and kids and are making a big impact. Don't forget that")
-insert_compliment('Smathers', 'Patrick', "There are a lot of days when I'm really stressed and overloaded, and simply even a simple encouraging text from you can make me feel so much better. So I hope that this message can help do the same for you <3")
-insert_compliment('Smathers', 'Patrick', "Do you remember when we planned our mountain trip? We were just thinking a nice getaway to the mountains and brevard would be nice, but we had no idea that that night we would tell each other three incredible words for the first time. Truly the best day of my life <3")
-insert_compliment('Smathers', 'Patrick', "I think it's so sweet and incredible that I found someone who is also so close to her family. I'm happy to find someone who also talks to their family every night and likes to visit them!")
-insert_compliment('Collins', 'Katelynne', "You are such a beautiful person! I hope you see that each and every day :D")
-insert_compliment('Collins', 'Katelynne', "I'm so thankful you made the group chat! You were able to unite so many people together and I'm so grateful for it!")
-insert_compliment('Collins', 'Katelynne', "You are such a wonderful friend! I'm always here for you!")
-insert_compliment('Collins', 'Katelynne', "I'm thankful for our memories together :D")
-insert_compliment('Collins', 'Katelynne', "You are so kind and funny! You always make me smile!")
-insert_compliment('Collins', 'Katelynne', "You are so hardworking and we are all proud of what you've done! Please remember to take breaks, you deserve it!")
-
+# insert_compliment('Smathers', 'Patrick', f"Hi Alana :)\nI hope you know just how sweet and amazing you are. You help so many families and kids and are making a big impact. Don't forget that")
+# insert_compliment('Smathers', 'Patrick', "There are a lot of days when I'm really stressed and overloaded, and simply even a simple encouraging text from you can make me feel so much better. So I hope that this message can help do the same for you <3")
+# insert_compliment('Smathers', 'Patrick', "Do you remember when we planned our mountain trip? We were just thinking a nice getaway to the mountains and brevard would be nice, but we had no idea that that night we would tell each other three incredible words for the first time. Truly the best day of my life <3")
+# insert_compliment('Smathers', 'Patrick', "I think it's so sweet and incredible that I found someone who is also so close to her family. I'm happy to find someone who also talks to their family every night and likes to visit them!")
+# insert_compliment('Collins', 'Katelynne', "You are such a beautiful person! I hope you see that each and every day :D")
+# insert_compliment('Collins', 'Katelynne', "I'm so thankful you made the group chat! You were able to unite so many people together and I'm so grateful for it!")
+# insert_compliment('Collins', 'Katelynne', "You are such a wonderful friend! I'm always here for you!")
+# insert_compliment('Collins', 'Katelynne', "I'm thankful for our memories together :D")
+# insert_compliment('Collins', 'Katelynne', "You are so kind and funny! You always make me smile!")
+# insert_compliment('Collins', 'Katelynne', "You are so hardworking and we are all proud of what you've done! Please remember to take breaks, you deserve it!")
+# insert_compliment('Pagilagan', 'Karl', "If you ever need anything we're a call away :)")
+insert_compliment('Pagilagan', 'Karl', "You know if things get kinda tough, we could always hang out to relieve some stress 👀 You, patty, and I could go for another din din night 👀👀")
+# insert_compliment('Pagilagan', 'Karl', "I couldn't really think of another inspirational quote, so instead here's a quote from Yoko Ono that I think you'd like. \"AAAAHHHhahHahahahHhuuhhHWHOoooooOooOoOoOoOooOoo\"")
+# insert_compliment('Taylor', 'Caden', "Thanks for seeing something in me at the protest! Now I've never been more grateful to have such a wonderful friend!")
+# insert_compliment('Taylor', 'Caden', "Who could ask for a better person to be queen of the Clemson Friends")
+# insert_compliment('Taylor', 'Caden', "I wanna make a joke about Freud but my ego is getting the best of me")
 
 
 connection.commit()
