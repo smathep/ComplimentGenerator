62184a138b3ae9c8a96b05205e050066d3347d7c
Patrick Smathers <patricksmathers@icloud.com>
2022-12-22 04:22:18 +0000

    Added corinne's comments and updated some stuff

--- .start.sh
+++ .start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 source venv/bin/activate
-python3 init_db.py
+# python3 init_db.py
+# DB Should already be initialized
 # export FLASK_APP=app
 # EXPORT FLASK_ENV=development
 # flask run
--- development.sh
+++ development.sh
@@ -1,7 +1,8 @@
 export FLASK_APP=app
 export FLASK_ENV=development
 source venv/bin/activate
-python3 init_db.py
+# python3 init_db.py
+#DB Should already be initialized
 flask run
 
 deactivate
